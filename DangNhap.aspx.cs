1b1f1a280ffc32d9b3620869d2af890556b9794f
honguyen1608 <53283990+honguyen1608@users.noreply.github.com>
2019-09-24 21:29:27 +0700

    Add files via upload

--- DangNhap.aspx.cs
+++ DangNhap.aspx.cs
@@ -6,13 +6,32 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class DangNhap : System.Web.UI.Page
+public partial class _Default : System.Web.UI.Page
 {
     LopXLDuLieu xl = new LopXLDuLieu();
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
-   
 
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        string[] vals = new string[]{
+                txtLogin.Text,
+                txtPass.Text           
+            };
+        string[] parar = new string[]{
+                "@TEN_DN",
+                "@MAT_KHAU"
+            };
+        DataTable ds = xl.docNhieuDL("TAIKHOAN", vals, parar);
+        if (ds.Rows.Count == 1)
+        {
+            Session["taikhoan"] = txtLogin.Text;
+            Response.Redirect("ViewDestinations.aspx");
+        }
+
+        else
+            lblThongBao.Text = "Sai tài khoản hoặc mật khẩu!";
+    }
 }
